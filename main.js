81170c46600c7957b92464e42bb56b6df570ecae
Dev-CyRus <76878654+Dev-CyRus@users.noreply.github.com>
2021-02-05 18:17:14 +0600

    Add files via upload
    First upload

---
+++ main.js
@@ -0,0 +1,28 @@
+const Discord = require('discord.js');
+
+const client = new Discord.Client();
+
+const prefix = '+';
+
+client.once('ready', () =>{
+    console.log('Poison is Online!');
+});
+
+client.on('message' , message =>{
+    if(!message.content.startsWith(prefix) || message.author.bot) return;
+
+    const args = message.content.slice(prefix.length).split(" ");
+    const command = args.shift().toLowerCase();
+
+    if (command === 'ping'){
+        message.channel.send('pong!');
+    } else if (command == 'hi'){
+        message.channel.send('Hello dear, How are you?');
+    } else if (command == 'youtube'){
+        message.channel.send('https://www.youtube.com/channel/UCqKJ0NnfNEk-j8pPzDe8CFw?view_as=subscriber');
+    } else if (command =='invite'){
+        message.channel.send('https://discord.gg/6G4bSMzaw4');
+    }
+});
+
+client.login(process.env.TOKEN);
