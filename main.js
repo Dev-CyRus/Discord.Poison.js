c23999be6ea68cede6409fff79eab7e2d93ad3c3
Dev-CyRus <76878654+Dev-CyRus@users.noreply.github.com>
2021-02-23 18:11:06 +0600

    welcome role added

--- main.js
+++ main.js
@@ -25,6 +25,11 @@ client.once('ready', () =>{
     client.user.setActivity("The prefix is +")
 });
 
+client.on('guildMemberAdd', guildMember =>{
+    let welcomeRole = guildMember.guild.roles.cache.find(role => role.name === '▬▬▬ 【 ᴜɴᴛʀᴜꜱᴛᴇᴅ 】▬▬▬');
+
+    guildMember.roles.add(welcomeRole)
+});
 client.on('message' , message =>{
     if(!message.content.startsWith(prefix) || message.author.bot) return;
 
