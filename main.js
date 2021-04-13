a87656d54e72a7b00ca5b361c817bdf8a4d95b1e
Glitch (poison-js) <none>
2021-04-13 16:30:42 +0000

    Added auto changing status

--- main.js
+++ main.js
@@ -17,7 +17,20 @@ for(const file of commandFiles){
 
 client.once('ready', () =>{
     console.log('Poison is Online!')
-    client.user.setActivity("Type +ticket for help")
+});
+
+const activities_list = [
+    "Type +ticket for help", 
+    "Use +play to play any music you want",
+    "Getting developed by CyRus", 
+    "Use +youtube to get Cyrus's youtube channel"
+    ]; // creates an arraylist containing phrases you want your bot to switch through.
+
+client.on('ready', () => {
+    setInterval(() => {
+        const index = Math.floor(Math.random() * (activities_list.length - 1) + 1); // generates a random number between 1 and the length of the activities array list (in this case 5).
+        client.user.setActivity(activities_list[index]); // sets bot's activities to one of the phrases in the arraylist.
+    }, 10000); // Runs this every 10 seconds.
 });
 
 client.on('guildMemberAdd', guildMember => {
