56d606793ab5f48c7703242facdf5f95b17fdde8
Glitch (poison-js) <none>
2021-04-23 18:06:38 +0000

    Added canvas

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
 const Discord = require('discord.js');
 
+const Canvas = require('canvas');
+
 const client = new Discord.Client();
 
 const prefix = '+';
@@ -33,24 +35,60 @@ client.on('ready', () => {
     }, 10000); // Runs this every 10 seconds.
 });
 
-client.on('guildMemberAdd', guildMember => {
-    let welcomeRole = guildMember.guild.roles.cache.find(role => role.name === '▬▬▬ 【 untrusted 】▬▬▬');
+client.on('guildMemberAdd', async member => {
+	const channel = member.guild.channels.cache.find(ch => ch.name === '│🚾│ᴡᴇʟᴄᴏᴍᴇ');
+	if (!channel) return;
 
-    guildMember.roles.add(welcomeRole);
-    guildMember.guild.channels.cache.get('780499363369713705').send(`
-━━━━━━━━━━━━━━━━━━━━━━━━
-              【 <a:ac:831413092775886891> 】 ****POISON'S LABORATORY****  【 <a:ac:831413092775886891> 】
-━━━━━━━━━━━━━━━━━━━━━━━━
-> Greetings  <@${guildMember.user.id}>, 
-> Welcome to POISON'S Official Discord Server
-**__Be sure to__** :
-━━━━━━━━━━━━━━━━━━━━━━━━
-<a:Pin:783924500063453186>│<a:arrow:783926999302799411> Please react ✅ on <#784811207784923136>
-<a:emg:795351407452618753>│<a:arrow:783926999302799411> Please read <#780500519659503656>
-<a:dc:795351406017118318>│<a:arrow:783926999302799411> Don't forget to take **role** from <#780707732721500161>
-<a:starr:795351408274046996>│<a:arrow:783926999302799411> Use <#780488699708047420> for ***chatting***
-━━━━━━━━━━━━━━━━━━━━━━━━`)
+	const canvas = Canvas.createCanvas(700, 250);
+	const ctx = canvas.getContext('2d');
+
+	const background = await Canvas.loadImage('./wallpaper.jpg');
+	ctx.drawImage(background, 0, 0, canvas.width, canvas.height);
+
+	ctx.strokeStyle = '#74037b';
+	ctx.strokeRect(0, 0, canvas.width, canvas.height);
+
+	// Slightly smaller text placed above the member's display name
+	ctx.font = '28px sans-serif';
+	ctx.fillStyle = '#ffffff';
+	ctx.fillText('Welcome to the server,', canvas.width / 2.5, canvas.height / 3.5);
+
+	// Add an exclamation point here and below
+	ctx.font = applyText(canvas, `${member.displayName}!`);
+	ctx.fillStyle = '#ffffff';
+	ctx.fillText(`${member.displayName}!`, canvas.width / 2.5, canvas.height / 1.8);
+
+	ctx.beginPath();
+	ctx.arc(125, 125, 100, 0, Math.PI * 2, true);
+	ctx.closePath();
+	ctx.clip();
+
+	const avatar = await Canvas.loadImage(member.user.displayAvatarURL({ format: 'jpg' }));
+	ctx.drawImage(avatar, 25, 25, 200, 200);
+
+	const attachment = new Discord.MessageAttachment(canvas.toBuffer(), 'welcome-image.png');
+
+	channel.send(`Welcome to the server, ${member}!`, attachment);
 });
+
+//client.on('guildMemberAdd', guildMember => {
+//    let welcomeRole = guildMember.guild.roles.cache.find(role => role.name === '▬▬▬ 【 untrusted 】▬▬▬');
+
+//    guildMember.roles.add(welcomeRole);
+//    guildMember.guild.channels.cache.get('780499363369713705').send(`
+//━━━━━━━━━━━━━━━━━━━━━━━━
+//              【 <a:ac:831413092775886891> 】 ****POISON'S LABORATORY****  【 <a:ac:831413092775886891> 】
+//━━━━━━━━━━━━━━━━━━━━━━━━
+//> Greetings  <@${guildMember.user.id}>, 
+//> Welcome to POISON'S Official Discord Server
+//**__Be sure to__** :
+//━━━━━━━━━━━━━━━━━━━━━━━━
+//<a:Pin:783924500063453186>│<a:arrow:783926999302799411> Please react ✅ on <#784811207784923136>
+//<a:emg:795351407452618753>│<a:arrow:783926999302799411> Please read <#780500519659503656>
+// <a:dc:795351406017118318>│<a:arrow:783926999302799411> Don't forget to take **role** from <#780707732721500161>
+// <a:starr:795351408274046996>│<a:arrow:783926999302799411> Use <#780488699708047420> for ***chatting***
+// ━━━━━━━━━━━━━━━━━━━━━━━━`)
+//});
 
 client.on('message' , message =>{
     if(!message.content.startsWith(prefix) || message.author.bot) return;
